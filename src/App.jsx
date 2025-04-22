0bfa39a812ebf4a4863cebee6610b4ac249d95c0
André Melchior <andre.melchior@outlook.com>
2025-04-21 23:52:02 -0300

    feat: add props in "Post" component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { Header } from "./components/Header"
 
 export function App() {
   return (
-    <div>
-      <Header />
-      <h1>Hello, World!</h1>
-      <Post />
-      <Post />
-      <Post />
-    </div>
-    
-  )
+      <div>
+          <Header />
+          <h1>Hello, World!</h1>
+
+          <Post author="André" content="lorem ipsum dolor sit amet." />
+
+          <Post author="Lucas" content="lorem ipsum."/>
+      </div>
+  );
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,3 +1,8 @@
-export function Post(){
-    return <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis incidunt quaerat aspernatur, iste non qui quo deserunt sequi eos corrupti eum perferendis porro animi blanditiis? Omnis illum non laborum animi.</p>
+export function Post(props){
+    return (
+        <div>
+            <strong>{props.author}</strong>
+            <p>{props.content}</p>
+        </div>
+    );
 }
